5815bd87da0b3f260a98a3c163def5af1724fea0
Al <al@metricmath.com>
2021-04-29 19:56:51 -0700

    added comments and generalization for x interval

--- Approx.py
+++ Approx.py
@@ -14,39 +14,43 @@ func_str = '$x^2$'
 
 fit = nf.neuroFit()
 
-m = 20  # number of neurons
+m = 3  # number of neurons
 n = 200 # number of points
 
-# generate model
+# generate model using m neurons
 
-x_fit = np.linspace(-1, 1, m)
+a = -2
+b = 2
+
+x_fit = np.linspace(a, b, m)
 y_fit = func(x_fit)
 
-# compute weights and biases analytically
+# compute weights and biases analytically for relu activation functions
 
 coeffs = fit.reluFit(m, x_fit, y_fit)
 
-# generate function and approximation at n points in the interval [-1, 1]
+# generate function and approximation at n points in the interval [a, b]
 
-x = np.linspace(-1, 1, n)
+x = np.linspace(a, b, n)
 y = func(x)
+
+
 func_r = fit.reluReconst(coeffs, x)
 
-# compute mean squared error in the interval
+# compute mean squared error in the interval [a, b]
  
 MSE = (1/n)*np.sum((y - func_r)**2)
 
-# rescale x to show what happens outside of [-1, 1]
+# rescale x to show what happens outside of [a, b]
 
-x1 = np.linspace(-2, 2, 2*n)
+x1 = np.linspace(2*a, 2*b, 2*n)
 y1 = func(x1)
 func_r1 = fit.reluReconst(coeffs, x1)
 
+y_min = min(func_r1)
+y_max = max(func_r1)
 
 # plot results
 
-fplot = nf.fitPlot((-3, 3), (-2, 2), func_str, m, n, MSE, 30, 40)
+fplot = nf.fitPlot((2*a, 2*b), (y_min - 1, y_max + 1), func_str, m, n, MSE, 30, 40)
 fplot.plot(x1, y1, func_r1)
-
-
-
